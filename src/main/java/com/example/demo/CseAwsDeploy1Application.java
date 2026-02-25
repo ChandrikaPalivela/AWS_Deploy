6ff1dd825d42d40fd6aae103e03261f36983255f
ChandrikaPalivela <23MH1A05J9@acoe.edu.in>
2026-02-25 10:16:56 +0530

    Initial commit

---
+++ src/main/java/com/example/demo/CseAwsDeploy1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CseAwsDeploy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CseAwsDeploy1Application.class, args);
+	}
+
+}
